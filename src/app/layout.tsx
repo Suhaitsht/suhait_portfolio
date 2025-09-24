705882f8ade6fe7aa6eca89b1d1bb0eab982436d
Suhaitsht <nm.suhaitsuhait@gmail.com>
2025-09-24 16:32:51 +0530

    modifed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavaBar from "./components/NavaBar";
+import NavaBar from "./components/NavBar";
 import { ThemeProvider } from "./context/ThemeContext";
 
 const geistSans = Geist({
