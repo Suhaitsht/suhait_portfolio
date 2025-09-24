110f18b5de979017e38fe3181fddcddd1806b410
Suhaitsht <nm.suhaitsuhait@gmail.com>
2025-09-24 16:30:28 +0530

    import a context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavaBar from "./components/NavaBar";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white transition-colors dark:bg-gray-900 dark:text-white`}
       >
-        <NavaBar />
-        {children}
+        <ThemeProvider>
+          <NavaBar /> <main className="min-h-screen pt-24">{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
