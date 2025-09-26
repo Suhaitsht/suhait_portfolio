f637ecd66652da611c37b88f38b8fa7d0d2b1ac7
Suhaitsht <nm.suhaitsuhait@gmail.com>
2025-09-26 14:04:43 +0530

    modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Hero from "./components/Hero";
-
+import Projects from "./components/Projects";
+import Blogs from "./components/Blogs";
+import Newsletter from "./components/Newsletter";
 export default function page() {
   return (
     <>
       <Hero />
+      <Projects />
+      <Blogs />
+      <Newsletter />
     </>
   );
 }
